a19342931898e56884a60f8e03a001d0b9d0309c
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-12-04 21:41:10 +0100

    add grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/_main.scss'
+import './App.scss'
 import { Nav } from './components/Nav'
 import { Login, Register } from './components/Auth'
 import Home from './components/Home'
@@ -8,12 +9,14 @@ import Home from './components/Home'
 const App = () => {
   return (
     <Router>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-      </Routes>
+      <div className="container">
+        <Nav />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
