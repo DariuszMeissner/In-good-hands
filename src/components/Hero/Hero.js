fadc8d16b67553f85a51547ef8527c02de6b89a2
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-12-05 12:59:15 +0100

    header-hero, breakpoint mixin

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Button } from '../Button'
+import { ReactComponent as DecorationSvg } from '../../assets/images/Decoration.svg'
+import HeroImage from '../../assets/images/Home-Hero-Image.jpg'
+import './Hero.scss'
 
 const Hero = () => {
     return (
         <>
-            <h1>Start helping! Put your unwanted items in trusted hands</h1>
-            <i></i>
-            <div>
-                <Button
-                    to='/sign-in'
-                    name='hand over the things'></Button>
-                <Button
-                    to='/sign-in'
-                    name='organize a fundraiser'></Button>
+            <div className="row hero">
+                <div className="col-sm-6">
+                    <img className="hero__image" src={HeroImage} alt="hero" />
+                </div>
+                <div className="col-sm-6 hero__cta">
+                    <div className="hero__box">
+                        <h1>Start helping! Put your unwanted items in trusted hands</h1>
+                        <DecorationSvg />
+                        <div>
+                            <Button
+                                to='/sign-in'
+                                name='hand over the things'></Button>
+                            <Button
+                                to='/sign-in'
+                                name='organize a fundraiser'></Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
