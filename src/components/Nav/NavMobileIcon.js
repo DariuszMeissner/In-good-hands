cb84afc4f527605bcf5ebbb521e06cd8df279e34
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-12-27 18:43:50 +0100

    refactored of using icons

--- src/components/Nav/NavMobileIcon.js
+++ src/components/Nav/NavMobileIcon.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { ReactComponent as BarsIcon } from '../../assets/icons/bars.svg'
 import './NavMobileIcon.scss'
 
 export const NavMobileIcon = ({ onClick }) => {
     return (
         <>
-            <BarsIcon className="icon-bars" onClick={onClick}>Icon</BarsIcon>
+            <div className="icon-bars" onClick={onClick}></div>
         </>
     )
 }
