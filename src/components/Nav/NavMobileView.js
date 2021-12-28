39e59d3ad6b741217a7a344586306a1eefaf54e4
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-12-28 12:27:13 +0100

    added transition for hamburger menu by react-transition-group

--- src/components/Nav/NavMobileView.js
+++ src/components/Nav/NavMobileView.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { NavAuth, NavMain } from '.'
 import './NavMobileView.scss'
+import { ClientSize } from '../UI/clientSize'
 
-export const NavMobileView = ({ onClick }) => {
+
+
+export const NavMobileView = ({ onClick, show }) => {
+    const { isS, isM, isL, isXL } = ClientSize();
+
+    const cssClasses = [
+        "navMobileView",
+        show === 'entering' || show === 'entered'
+            ? "show-up"
+            : show === 'exiting' ? "hide-up" : null,
+
+        isS
+            ? "mobile-width" 
+            : isM ? "tablet-width" : null
+    ]
+
     return (
-        <div className='navMobileView'>
+        <div className={cssClasses.join(' ')}>
             <NavAuth />
             <NavMain />
             <button onClick={onClick}>Close</button>
