19e1331af9175606116c22c567a0c097069c295b
DariuszMeissner <dariusz.r.meissner@gmail.com>
2021-12-28 13:32:41 +0100

    corrected font-size atomic breakpointname

--- src/components/Nav/NavAuth.js
+++ src/components/Nav/NavAuth.js
@@ -4,7 +4,7 @@ import './NavAuth.scss'
 
 export const NavAuth = () => {
     return (
-        <ul className='flex-l m-b-4 fs-3-l'>
+        <ul className='flex-l m-b-4 fs-1-l'>
             <li><Link to="/sign-in">Sign In</Link></li>
             <li><Link to="/register">Register</Link></li>
         </ul>
